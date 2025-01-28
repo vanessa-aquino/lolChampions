8a2b73e4ea9eb14d5803abd4ff36825c7db27d0a
Vanessa <vanessaquinoo@hotmail.com>
2025-01-28 11:14:01 -0300

    fix: altarado a url para https

--- src/app/services/champions.service.ts
+++ src/app/services/champions.service.ts
@@ -7,8 +7,8 @@ import { Observable } from 'rxjs';
 })
 
 export class ChampionsService {
-  private apiUrl = 'http://ddragon.leagueoflegends.com/cdn/15.2.1/data/pt_BR/champion.json';
-  private baseUrl = 'http://ddragon.leagueoflegends.com/cdn/15.2.1/data/pt_BR/champion/';
+  private apiUrl = 'https://ddragon.leagueoflegends.com/cdn/15.2.1/data/pt_BR/champion.json';
+  private baseUrl = 'https://ddragon.leagueoflegends.com/cdn/15.2.1/data/pt_BR/champion/';
 
   constructor(private http: HttpClient) { };
 
